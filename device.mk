4ea5b964f0d49f75239522b8a410aea6774b335b
Bruno Martins <bgcngm@gmail.com>
2018-04-09 23:07:57 +0100

    griffin: Drop USB HAL service
    * griffin doesn’t have support for dual role type-C,
       therefore there's no need to build it.
    
    Change-Id: Ie5b135b8920ae255a2054a04cb7326c68abad0cd
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -383,10 +383,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp \
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.lineage
